383c6b0df69dd561b7d3d7d8ba57fc7351f4a204
Jeff Kempista <jeffkempista@icloud.com>
2014-11-15 08:51:25 -0500

    Initial Commit

---
+++ LocationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationExample
+//
+//  Created by Jeff Kempista on 11/15/14.
+//  Copyright (c) 2014 Jeff Kempista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
